6102a2183deacbd560c7422b27fcb52b385ce840
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-06-09 16:35:27 +0300

    Added: UI,implementation adaptive

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,124 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <v-app>
+                <v-navigation-drawer
+                        v-model="drawer"
+                        app
+                        disable-resize-watcher
+                >
+                    <v-list shaped>
+                        <v-subheader>Market Place</v-subheader>
+                        <v-list-item-group color="primary">
+                            <v-list-item
+                                    v-for="(link_list,i) in links"
+                            :key="i"
+                            :to="link_list.url">
+                                <v-list-item-icon left>
+                                            <v-icon :color="link_list.color">{{link_list.icon}}</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-content>
+                                    <v-list-item-title v-text="link_list.title"></v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list-item-group>
+                    </v-list>
+                </v-navigation-drawer>
+                <v-app-bar
+                        app
+                        clipped-left
+                >
+                    <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
+                    <v-toolbar-title> <v-icon left color="red">mdi-store</v-icon>Market Place</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items class="hidden-sm-and-down">
+                        <v-btn   v-for="(link_list,i) in links"
+                                 :key="i"
+                                 :to="link_list.url">
+                            <v-icon left :color="link_list.color">{{link_list.icon}}</v-icon>
+                            {{link_list.title}}
+                        </v-btn>
+                    </v-toolbar-items>
+                </v-app-bar>
+                <v-content>
+                    <v-container
+                            class="fill-height"
+                            fluid
+                    >
+                        <v-row
+                                align="center"
+                                justify="center"
+                        >
+                        </v-row>
+                    </v-container>
+                </v-content>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+                <v-footer
+                        color="dark lighten-1"
+                        padless
+                >
+                    <v-row
+                            justify="center"
+                            no-gutters
+                    >
+                        <v-col
+                                class=" lighten-2 py-1 text-center white--text"
+                                cols="14"
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+                        >
+                            <a :href="footer_link_info.user_profile" :target="footer_link_info.target">
+                            <v-avatar>
+                                <img
+                                        :src="images_git_hub"
+                                        alt="GitHub"
+                                >
+                            </v-avatar>
+                        </a>
+                        </v-col>
+                    </v-row>
+                </v-footer>
+        </v-app>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld';
+    export default {
+        data: () => ({
+            drawer: false,
+            footer_link_info: {'user_profile': "https://github.com/BootleggerHigh", target: "_blank"},
+            images_git_hub: require('./static/images/GitHub.png'),
+            links:[
+                {
+                  title:'Авторизация',
+                  icon:'mdi-account-arrow-left',
+                    color:'green',
+                    url:'/login',
+                },
+                {
+                    title:'Регистрация',
+                    icon:'mdi-account-convert',
+                    color:'blue',
+                    url:'/registration',
+                },
+                {
+                    title:'Мои заказы',
+                    icon:'mdi-basket',
+                    color:'yellow',
+                    url:'/orders',
+                },
+                {
+                    title:'Новые товары',
+                    icon:'mdi-cart-plus',
+                    color:'orange',
+                    url:'/new',
+                },
+                {
+                    title:'Мои товары',
+                    icon:'mdi-cart-arrow-right',
+                    color:'purple',
+                    url:'/list',
+                },
+            ]
+        }),
 
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+        created() {
+            this.$vuetify.theme.dark = true
+        },
+    }
 </script>
