f6c9f8755503a9af2db9b7ff6ef88eb4fc9fbbf6
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-06-10 22:25:52 +0300

    Added: UI: flash any errors

---
+++ src/components/flash/snackbar_error.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-snackbar
+            :multi-line="true"
+            :top="true"
+            :timeout="6000"
+            color="error"
+            @input="closeError"
+            :value="true"
+    >
+        {{ error_message }}
+        <v-btn
+                dark
+                text
+                @click.native="closeError"
+        >
+            Закрыть
+        </v-btn>
+    </v-snackbar>
+</template>
+
+<script>
+    export default {
+        name: "snackbar_error",
+        methods:{
+            closeError()
+            {
+                this.$store.dispatch('clearError');
+            },
+        },
+        computed:
+            {
+                error_message()
+                {
+                    return this.$store.getters.error;
+                }
+            }
+    }
+</script>
+
+<style scoped>
+
+</style>
