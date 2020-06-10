c1d1a1b16d3cbe27151172bca3c71a87ab852744
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-06-10 22:33:21 +0300

    Update: connect firebas and route,store

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import * as fb from 'firebase'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,19 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created()
+  {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBdgJ2nDmVHuCXIdY5wGC8Kpt1KtIuxizU",
+      authDomain: "vue-start-project.firebaseapp.com",
+      databaseURL: "https://vue-start-project.firebaseio.com",
+      projectId: "vue-start-project",
+      storageBucket: "vue-start-project.appspot.com",
+      messagingSenderId: "412659603744",
+      appId: "1:412659603744:web:9237b3a9c3137db85d1f45",
+      measurementId: "G-NJT686ZRKS"
+    }
+    fb.initializeApp(firebaseConfig);
+  }
 }).$mount('#app')
