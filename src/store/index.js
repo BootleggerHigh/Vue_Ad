2214ac82a06fe8fe17da8a8ebc20c0e0c104dbc0
Bootlegger_Dmitry <61204780+BootleggerHigh@users.noreply.github.com>
2020-06-10 22:31:19 +0300

    Update: index store add new modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-Vue.use(Vuex)
+import ads_store from "./ads_store";
+import user_store from "./user_store";
+import comma from "./comma";
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
@@ -11,5 +13,8 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    ads_store,
+    user_store,
+    comma,
   }
 })
